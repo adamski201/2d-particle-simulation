3e64c2b8beabf055722058244fc0b2a007e66a06
adamski201 <lanster993@hotmail.com>
2021-10-14 13:49:06 +0100

    minor improvements to plotting function

--- projectile_package/projectile_plot.py
+++ projectile_package/projectile_plot.py
@@ -1,6 +1,9 @@
 import matplotlib.pyplot as plt
 
-def plot_xy(x, y, time):
+def plot_xy(x, y):
     
     fig, ax = plt.subplots()
     ax.plot(x, y)
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    

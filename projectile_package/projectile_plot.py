7be6bb7de8e713be31c234abde2ca8e7d404eeca
adamski201 <lanster993@hotmail.com>
2021-10-14 14:39:24 +0100

    added comments, axes labels

--- projectile_package/projectile_plot.py
+++ projectile_package/projectile_plot.py
@@ -1,9 +1,14 @@
 import matplotlib.pyplot as plt
 
 def plot_xy(x, y):
+    # create a scatter graph using matplotlib
+    
     
     fig, ax = plt.subplots()
     ax.plot(x, y)
-    plt.xlabel("X")
-    plt.ylabel("Y")
+    
+    # add axis labels and a title for clarity
+    plt.xlabel("X / m")
+    plt.ylabel("Y / m")
+    plt.title("Trajectory of particle in 2 dimensions")
     
